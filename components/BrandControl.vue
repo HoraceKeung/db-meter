065c4d0b541f5b6dfbf69c7b68d49e592442c5c5
Horace Keung <horacekeung@hotmail.com>
2018-05-01 00:11:04 +0100

    added overlay

--- components/BrandControl.vue
+++ components/BrandControl.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="card bg-light p-3 mb-3">
 		<div class="row">
-			<div class="col-sm-6 col-md-4 col-lg-6 mb-3">
+			<div class="col mb-3">
 				<label>Background color</label>
 				<chrome v-model="color"/>
 			</div>
-			<div class="col-sm-6 col-md-8 col-lg-6">
+			<div class="col">
 				<label>Logo</label>
 				<div><croppa v-model="crop" @draw="draw" @new-image-drawn="draw" @image-remove="$store.commit('setLogo', null)" :width="250" :height="250" :zoom-speed="30" remove-button-color="#dc3545"/></div>
 			</div>
--- components/OutputDisplay.vue
+++ components/OutputDisplay.vue
@@ -1,7 +1,14 @@
 <template>
 	<div class="border" :style="'background-color: '+rgba+';'">
-		<img v-if="$store.state.logo" width="100" height="100" class="border" :src="$store.state.logo">
-		<span class="text-white">{{measuring?decibel:maxRecord}}dB</span>
+		<img class="w-100" src="~/assets/img/db_meter_overlay.png">
+		<div class="position-relative">
+			<div class="position-absolute w-100 text-center" style="bottom: 0;">
+				<img v-if="$store.state.logo" style="width: 10%;" :src="$store.state.logo">
+			</div>
+			<div class="position-absolute w-100 d-flex" style="bottom: 0;">
+				<h1 class="db-text text-white ml-auto mr-3">{{measuring?decibel:maxRecord}}dB</h1>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -23,3 +30,6 @@ export default {
 	}
 }
 </script>
+
+<style scoped>
+</style>
